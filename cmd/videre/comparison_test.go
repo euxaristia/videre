304d47ca979d75f1d3a1555fa929e6500ec08001
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-02-15 20:31:01 -0500

    Port Neovim comparison benchmarks to Go using PTY automation

---
+++ cmd/videre/comparison_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func checkNvim(b *testing.B) {
+	if _, err := exec.LookPath("nvim"); err != nil {
+		b.Skip("nvim not found, skipping comparison")
+	}
+}
+
+func runComparison(b *testing.B, name string, testFunc func(b *testing.B, d EditorDriver)) {
+	b.Helper()
+	
+	b.Run(name+"/videre", func(b *testing.B) {
+		d := NewVidereDriver()
+		testFunc(b, d)
+	})
+
+	checkNvim(b)
+	b.Run(name+"/nvim", func(b *testing.B) {
+		d := NewNvimDriver()
+		testFunc(b, d)
+	})
+}
+
+func BenchmarkCompareStartup(b *testing.B) {
+	runComparison(b, "Startup", func(b *testing.B, d EditorDriver) {
+		for i := 0; i < b.N; i++ {
+			_, err := d.Start("")
+			if err != nil {
+				b.Fatal(err)
+			}
+			_, _ = d.Quit(true)
+		}
+	})
+}
+
+func BenchmarkCompareInsertion(b *testing.B) {
+	runComparison(b, "Insertion", func(b *testing.B, d EditorDriver) {
+		for i := 0; i < b.N; i++ {
+			_, err := d.Start("")
+			if err != nil {
+				b.Fatal(err)
+			}
+			_ = d.SendKeys("i", 0)
+			for j := 0; j < 10; j++ {
+				_ = d.SendKeys("hello ", 0)
+			}
+			_ = d.SendKeys("<ESC>", 0)
+			_, _ = d.Quit(true)
+		}
+	})
+}
+
+func BenchmarkCompareMovement(b *testing.B) {
+	// Generate a temporary file for movement tests
+	f, err := os.CreateTemp("", "bench_move_*.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+	for i := 0; i < 100; i++ {
+		f.WriteString("The quick brown fox jumps over the lazy dog.\n")
+	}
+	f.Close()
+
+	runComparison(b, "Movement", func(b *testing.B, d EditorDriver) {
+		for i := 0; i < b.N; i++ {
+			_, err := d.Start(f.Name())
+			if err != nil {
+				b.Fatal(err)
+			}
+			for j := 0; j < 10; j++ {
+				_ = d.SendKeys("jjjkkkwwbbe", 0)
+			}
+			_, _ = d.Quit(true)
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module videre-go
 
 go 1.25
+
+require github.com/creack/pty v1.1.24 // indirect
